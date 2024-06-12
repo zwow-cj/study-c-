35676d034cd604cbd34f185a212b4c4a12c33d07
zwow-cj <2804818334@qq.com>
2024-06-12 21:58:35 +0800

    改进

--- auto/auto.cpp
+++ auto/auto.cpp
@@ -1,17 +1,17 @@
 #include <iostream>
 #include <vector>
-using namespace std;
+
 int main(){
     auto i = 1;
-    vector<int> a = {1,2,3};
-    for (vector<int>::iterator one = a.begin(); one < a.end(); ++one){
-        cout << *one << " ";
+    std::vector<int> a = {1,2,3};
+    for (std::vector<int>::iterator one = a.begin(); one < a.end(); ++one){
+        std::cout << *one << " ";
     }
-    cout << endl;
+    std::cout << std::endl;
     //用auto代替vector<int>::iterator，方便。
     for (auto one = a.begin(); one < a.end(); ++one){ 
-        cout << *one << " ";
+        std::cout << *one << " ";
     }
-    cout << endl << i;
+    std::cout << std::endl << i;
     return 0;
 }
