e85a42889dc9dc87682a44fc215755fc465af199
zwow-cj <2804818334@qq.com>
2024-06-12 21:46:32 +0800

    解决issue#1

--- auto/auto.cpp
+++ auto/auto.cpp
@@ -1,6 +1,17 @@
 #include <iostream>
+#include <vector>
+using namespace std;
 int main(){
     auto i = 1;
-    std::cout << i;
+    vector<int> a = {1,2,3};
+    for (vector<int>::iterator one = a.begin(); one < a.end(); ++one){
+        cout << *one << " ";
+    }
+    cout << endl;
+    //用auto代替vector<int>::iterator，方便。
+    for (auto one = a.begin(); one < a.end(); ++one){ 
+        cout << *one << " ";
+    }
+    cout << endl << i;
     return 0;
 }
